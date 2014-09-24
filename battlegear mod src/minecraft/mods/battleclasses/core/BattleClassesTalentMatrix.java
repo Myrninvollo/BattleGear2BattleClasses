32df6d293f4a41bfe608d9cec9be567b67a232f6
Zsolt Molnár <zsolt.molnarrr@gmail.com>
2014-09-25 01:00:10 +0200

    Added packet for talent node, tree and reset button selection

--- battlegear mod src/minecraft/mods/battleclasses/core/BattleClassesTalentMatrix.java
+++ battlegear mod src/minecraft/mods/battleclasses/core/BattleClassesTalentMatrix.java
@@ -1,10 +1,13 @@
 package mods.battleclasses.core;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import mods.battleclasses.ability.BattleClassesAbilityTest;
 import mods.battleclasses.ability.BattleClassesAbstractAbilityActive;
+import mods.battleclasses.ability.BattleClassesAbstractTalent;
 import mods.battleclasses.enumhelper.EnumBattleClassesPlayerClass;
+import mods.battleclasses.gui.controlls.BattleClassesGuiButtonTalentNode;
 
 public class BattleClassesTalentMatrix {
 	
@@ -15,15 +18,25 @@ public class BattleClassesTalentMatrix {
 	}
 	
 	public ArrayList<BattleClassesTalentTree> talentTrees = new ArrayList<BattleClassesTalentTree>();
+	public HashMap<Integer, BattleClassesAbstractTalent> talentHashMap = new HashMap<Integer, BattleClassesAbstractTalent>();
 
 	public static final int TALENT_POINTS_TO_SPEND = 3;
 	protected int talentPoints = TALENT_POINTS_TO_SPEND;
 	
 	public void initWithTalentTrees(ArrayList<BattleClassesTalentTree> parTalentTrees) {
+		//Setting reverse references on trees and talentAbilities
 		talentTrees = parTalentTrees;
 		for(BattleClassesTalentTree talentTree : talentTrees) {
 			talentTree.setOwnerTalentMatrix(this);
 		}
+		
+		//Filling up talentHashMap
+		talentHashMap.clear();
+		for(BattleClassesTalentTree talentTree : this.talentTrees) {
+        	for(BattleClassesAbstractTalent talentAbility : talentTree.talentList ) {
+        		this.talentHashMap.put(talentAbility.getAbilityID(), talentAbility);
+        	}
+        }
 	}
 	
 	public int getTalentPoints() {
